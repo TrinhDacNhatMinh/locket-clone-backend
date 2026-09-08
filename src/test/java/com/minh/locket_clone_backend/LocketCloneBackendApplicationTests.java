c600ef25fc074c2416818e85dd74c2d5c7328a8c
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2026-09-08 15:42:48 +0700

    first commit

---
+++ src/test/java/com/minh/locket_clone_backend/LocketCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.locket_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocketCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
