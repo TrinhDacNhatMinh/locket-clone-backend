55a9b9e534a1bb5d7ee3ca375e14ee07045fa120
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2026-09-08 21:18:03 +0700

    chore: setup local postgresql with docker compose and fix timezone

--- src/main/java/com/minh/locket_clone_backend/LocketCloneBackendApplication.java
+++ src/main/java/com/minh/locket_clone_backend/LocketCloneBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LocketCloneBackendApplication {
 
 	public static void main(String[] args) {
+		java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(LocketCloneBackendApplication.class, args);
 	}
 
